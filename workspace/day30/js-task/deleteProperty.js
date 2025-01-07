ebdc7ba4f3967a1cd5ce8fb0b2e61949b80bc486
jinsu734 <friend.723@daum.net>
2025-01-07 18:22:13 +0900

    250107 수업

---
+++ workspace/day30/js-task/deleteProperty.js
@@ -0,0 +1,10 @@
+function deleteProperty (tag){
+  tag.removeProperty('class');
+}
+
+// document.body.myData = {
+//   name: 'Caesar',
+//   title: 'Imperator'
+//  };
+
+//  alert(document.body.myData.title);
---
+++ workspace/day30/js-task/people.js
@@ -0,0 +1,12 @@
+let person1 = {
+  name: '짱구',
+  age: 5
+};
+
+let person2 = {
+  name: '유리',
+  age: 10
+};
+
+const arr = [person1, person2];
+console.log(arr);
